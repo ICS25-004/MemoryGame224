9e2f0e40019b58bb0ad4ddee6a1e8dfcab3a7a3b
Caleb Irvine <irvinecaleb1@gmail.com>
2026-01-05 10:52:53 -0800

    Initial Commit

---
+++ MemoryGame226/MemoryGame226App.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame226App.swift
+//  MemoryGame226
+//
+//  Created by Caleb on 2026-01-05.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGame226App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGame226Tests/MemoryGame226Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame226Tests.swift
+//  MemoryGame226Tests
+//
+//  Created by Caleb on 2026-01-05.
+//
+
+import Testing
+@testable import MemoryGame226
+
+struct MemoryGame226Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
