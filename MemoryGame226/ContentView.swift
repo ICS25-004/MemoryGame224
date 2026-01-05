3f8c2bc7bbb9604e56f3b5f8e6d3e5cd55124d74
Caleb Irvine <ICS25-004@camosun.ca>
2026-01-05 15:27:18 -0800

    added hstack

--- MemoryGame226/ContentView.swift
+++ MemoryGame226/ContentView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let suits: [String] = ["heart", "suit.spade", "suit.club", "diamonds"]
+    @State private var activeSuit = 2
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "arrowtriangle.left")
+                    .resizable()
+                    .foregroundStyle(.tint)
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture {
+                        activeSuit = (activeSuit - 1 + suits.count) % suits.count
+                    }
+                VStack {
+                    Image(systemName: suits[activeSuit])
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
+                        
+                    Text(suits[activeSuit].capitalized)
+                }
+                Image(systemName: "arrowtriangle.right")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(.tint)
+                    .onTapGesture {
+                        activeSuit = (activeSuit + 1) % suits.count
+                    }
+                    
+                    
+            }
         }
         .padding()
     }
@@ -22,3 +47,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
