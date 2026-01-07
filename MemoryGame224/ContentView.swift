9936d062b0f6dbbedaed789bf2a960dd49e33c3a
Caleb Irvine <ICS25-004@camosun.ca>
2026-01-06 16:09:37 -0800

    Add conditional suit colors

--- MemoryGame224/ContentView.swift
+++ MemoryGame224/ContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    let suits: [String] = ["heart", "suit.spade", "suit.club", "diamonds"]
+    private let suits: [String] = ["heart", "suit.spade", "suit.club", "diamond"]
     @State private var activeSuit = 2
+
+    private var suitColor: Color {
+        let name = suits[activeSuit]
+        return (name == "diamond" || name == "heart") ? .red : .black
+    }
+
     var body: some View {
         VStack {
             HStack{
@@ -24,20 +30,25 @@ struct ContentView: View {
                     Image(systemName: suits[activeSuit])
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .imageScale(.large)
-                        .foregroundStyle(.black)
-                        
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(suitColor)
+                        .padding(.bottom, 50)
+                    
                     Text(suits[activeSuit].capitalized)
-                }
+                        .frame(width: 200)
+                        .font(.system(size: 32, weight: .bold, design: .serif))
+                        .foregroundStyle(suitColor)
+                        .lineLimit(1)
+                }.padding(25)
                 Image(systemName: "arrowtriangle.right")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .foregroundStyle(.tint)
                     .onTapGesture {
                         activeSuit = (activeSuit + 1) % suits.count
+                        
                     }
-                    
-                    
             }
         }
         .padding()
