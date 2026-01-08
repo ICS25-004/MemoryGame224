ec692a72bde926f3cf92704a45277344b4fccf9b
Caleb Irvine <ICS25-004@camosun.ca>
2026-01-08 09:58:29 -0800

    Playing around with Styles

--- MemoryGame224/View/CardDetail.swift
+++ MemoryGame224/View/CardDetail.swift
@@ -8,28 +8,41 @@
 import SwiftUI
 
 struct CardDetail: View {
-	var body: some View {
-		TabView {
-			ForEach(Suit.allCases, id: \.self) { suit in
-				VStack{
-					Image(systemName: suit.iconName)
-						.resizable()
-						.scaledToFit()
-						.frame(width: 100)
-						.foregroundColor(suit.color)
-					
-					Text(suit.title)
-						.font(Font.custom("Monocraft", size: 32))
-						.foregroundColor(suit.color)
-				}
-			}
-		}
-		.tabViewStyle(.page(indexDisplayMode: .always))
-		.ignoresSafeArea()
-	}
+    var body: some View {
+        TabView {
+            ForEach(Suit.allCases, id: \.self) { suit in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(Color.white)
+                        .shadow(radius: 7)
+                        
+                    VStack{
+                        Image(systemName: suit.iconName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100)
+                            .foregroundColor(suit.color)
+                        
+                        Text(suit.title)
+                            .font(Font.custom("Monocraft", size: 32))
+                            .foregroundColor(suit.color)
+                    }
+                    .padding(.top, 30)
+                }
+                .padding(35)
+                .background(Color.accentColor)
+            }
+        }
+        .tabViewStyle(
+            .page(indexDisplayMode: .always)
+        )
+        .ignoresSafeArea()
+        .padding(.vertical, 50)
+        
+    }
 }
 
 #Preview {
-	CardDetail()
+    CardDetail()
 }
-	
+
